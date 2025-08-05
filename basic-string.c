d5df82330e5a63de89c0e1a417b2ecd5e7075b51
mrakib26 <mrakib2620@gmail.com>
2025-08-06 01:21:23 +0600

    first commit

---
+++ basic-string.c
@@ -0,0 +1,11 @@
+// We will learn basic string operations in C, including how to declare, initialize, and manipulate strings using standard library functions.
+
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    
+
+
+    return 0; 
+}
